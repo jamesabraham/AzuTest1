ff8a43fa8b27e1d4121ee25c165d2af591bc73e5
james.abraham <james.abraham@fingent.com>
2019-07-23 18:55:27 +0530

    Unit Test

---
+++ CtrlTest/ValueTest.cs
@@ -0,0 +1,21 @@
+using AzuTest1.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CtrlTest
+{
+    [TestClass]
+    public class ValueTest
+    {
+        [TestMethod]
+        public void TestValues()
+        {
+            //Arrange
+            //var controller = new ValuesController();
+
+            //Act
+
+            //Assert
+            Assert.AreEqual(1, 2);
+        }
+    }
+}
