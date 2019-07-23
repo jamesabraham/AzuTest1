58e273ef53659e91424d878d7411cb8e7cedbff1
james.abraham <james.abraham@fingent.com>
2019-07-23 17:03:42 +0530

    Value 3

--- AzuTest1/Controllers/ValuesController.cs
+++ AzuTest1/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace AzuTest1.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "value2" };
         }
 
         // GET api/values/5
